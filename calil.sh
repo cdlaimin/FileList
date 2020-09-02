07464fd0f46845373ad591f9c71b998200f189b5
Mikasa <33211138+cdlaimin@users.noreply.github.com>
2020-09-02 09:38:32 +0800

    Update calil.sh

--- calil.sh
+++ calil.sh
@@ -29,9 +29,8 @@ wget https://cdn.jsdelivr.net/gh/cdlaimin/v2ray-rules-dat@release/geoip.dat
 wget https://cdn.jsdelivr.net/gh/cdlaimin/v2ray-rules-dat@release/geosite.dat
 echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\n 两个文件用于V2ray的翻墙规则" > /root/gfwlist2pac/v2ray-rules-dat/README.md
 cd ..
-git add -all
 echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/gfwlist2pac/master/gfwlist.pac
 " > /root/gfwlist2pac/README.md
-git add .
+git add -all
 git commit -m "[$(LANG=C date +"%Y-%m-%d %H:%M:%S")]auto update"
 git push origin master
