531a68b231c3269c3bd5c958dd8877ca67c8bf91
cdlaimin <452959229@qq.com>
2021-09-09 09:02:19 +0800

    dd

--- calil.sh
+++ calil.sh
@@ -27,7 +27,7 @@ cd v2ray-rules-dat
 rm -rf geo*
 wget https://cdn.jsdelivr.net/gh/cdlaimin/v2ray-rules-dat@release/geoip.dat
 wget https://cdn.jsdelivr.net/gh/cdlaimin/v2ray-rules-dat@release/geosite.dat
-echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\n 两个文件用于V2ray的翻墙规则" > /root/PACList/v2ray-rules-dat/README.md
+echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\n 两个文件用于V2ray的翻墙规则" > /root/FileList/v2ray-rules-dat/README.md
 cd ..
 echo -e "最后一次更新时间 $(LANG=C date +"%Y-%m-%d %H:%M:%S")\nhttps://raw.githubusercontent.com/cdlaimin/FileList/master/PACList.pac
 " > /root/FileList/README.md
